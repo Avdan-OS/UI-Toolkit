b87d538ccbeda3013f64b6d31581cbf3424d9fa6
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-08 17:43:43 +0200

    Better file organization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use eframe::egui;
+#![allow(unused_imports)]
+use eframe::*;
 
 mod fonts;
+mod toolkit;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
@@ -8,27 +10,6 @@ fn main() {
     eframe::run_native (
         "AvdanOS toolkit",
         native_options,
-        Box::new(|cc| Box::new(UIToolkit::new(cc)))
+        Box::new(|cc| Box::new(toolkit::UIToolkit::new(cc)))
     );
-}
-
-#[derive(Default)]
-struct UIToolkit {}
-
-impl UIToolkit {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        fonts::setup_font(&cc.egui_ctx);
-
-        Self::default()
-    }
-}
-
-impl eframe::App for UIToolkit {
-   fn update(&mut self,
-             ctx:    &egui::Context,
-             _frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello, World!");
-       });
-   }
 }
