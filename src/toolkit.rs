3dae702ac2d9b628d0cba8110f5346a6fa66752b
Daniel D <kibalchishthecoder@gmail.com>
2022-08-23 22:26:01 +0300

    Implementing default members for UIToolkitDemo

--- src/toolkit.rs
+++ src/toolkit.rs
@@ -22,7 +22,7 @@ impl eframe::App for UIToolkitDemo {
             ui.heading(r#"AvdanOS UI Toolkit Demo"#);
             ui.end_row();
 
-            ui.separator();
+            ui.separator(); // visual separator (here it's a vertical line)
 
             ui.horizontal(|ui: &mut Ui| {
                 ui.label("Label: ");
@@ -42,5 +42,42 @@ impl eframe::App for UIToolkitDemo {
                 }
             });
         });
+    }
+
+    fn save(self: &mut UIToolkitDemo, _storage: &mut dyn eframe::Storage) {}
+
+    fn on_exit_event(&mut self) -> bool {
+        true
+    }
+
+    fn on_exit(&mut self, _gl: &eframe::glow::Context) {}
+
+    fn auto_save_interval(&self) -> std::time::Duration {
+        std::time::Duration::from_secs(30)
+    }
+
+    fn max_size_points(&self) -> egui::Vec2 {
+        egui::Vec2::INFINITY
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> egui::Rgba {
+        // NOTE: a bright gray makes the shadows of the windows look weird.
+        // We use a bit of transparency so that if the user switches on the
+        // `transparent()` option they get immediate results.
+        egui::Color32::from_rgba_unmultiplied(12, 12, 12, 180).into()
+
+        // _visuals.window_fill() would also be a natural choice
+    }
+
+    fn persist_native_window(&self) -> bool {
+        true
+    }
+
+    fn persist_egui_memory(&self) -> bool {
+        true
+    }
+
+    fn warm_up_enabled(&self) -> bool {
+        false
     }
 }
