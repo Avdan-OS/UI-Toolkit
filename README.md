b9bc95720c7d91ad637fe6ed83609e2453c422b4
Daniel D <kibalchishthecoder@gmail.com>
2022-08-24 22:36:45 +0300

    new features from the widget gallery

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # UI-Toolkit
+
 AvdanOS UI Toolkit made with egui
 
 ## Screenshots
+
 Light Mode
 ![Screenshot1](screenshots/light.png)
 
@@ -9,7 +11,9 @@ Dark Mode
 ![Screenshot1](screenshots/dark.png)
 
 ## How to use this
+
 ### Step 1: Setup
+
 On debian-based linux distros run this command on the terminal:
 
 ```sudo apt-get install -y libclang-dev libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev```
@@ -19,6 +23,7 @@ Meanwhile on fedora-based distros you need to run this command:
 ```sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel```
 
 ### Step 2: Building and running
+
 1. Clone this repository(`git clone https://github.com/Avdan-OS/UI-Toolkit.git`)
 
 2. Build the cloned repo(`cd UI-Toolkit && cargo run`)
@@ -26,4 +31,5 @@ Meanwhile on fedora-based distros you need to run this command:
 3. You're done!
 
 ### Useful Resources 
+
 Explore the different eGUI Widgets : `https://www.egui.rs/index.html#demo`
--- src/toolkit.rs
+++ src/toolkit.rs
@@ -1,11 +1,15 @@
 use eframe::egui;
+use eframe::egui::special_emojis::GITHUB;
 use eframe::egui::style::WidgetVisuals;
 
 use crate::egui::Ui;
 use crate::fonts::setup_font;
 
 #[derive(Default)]
-pub struct UIToolkitDemo;
+pub struct UIToolkitDemo{
+    scalar: f32,
+    animate_progress_bar: bool,
+}
 
 impl UIToolkitDemo {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -15,22 +19,91 @@ impl UIToolkitDemo {
     }
 }
 
+
+#[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+enum Enum {
+    First,
+    Second,
+    Third,
+}
+
+/// Shows off one example of each major type of widget.
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct WidgetGallery {
+    enabled: bool,
+    visible: bool,
+    boolean: bool,
+    radio: Enum,
+    scalar: f32,
+    string: String,
+    color: egui::Color32,
+    animate_progress_bar: bool,
+
+    #[cfg(feature = "chrono")]
+    #[cfg_attr(feature = "serde", serde(skip))]
+    date: Option<chrono::Date<chrono::Utc>>,
+
+    #[cfg_attr(feature = "serde", serde(skip))]
+    texture: Option<egui::TextureHandle>,
+}
+
+impl Default for WidgetGallery {
+    fn default() -> Self {
+        Self {
+            enabled: true,
+            visible: true,
+            boolean: false,
+            radio: Enum::First,
+            scalar: 42.0,
+            string: Default::default(),
+            color: egui::Color32::LIGHT_BLUE.linear_multiply(0.5),
+            animate_progress_bar: true,
+            texture: None,
+        }
+    }
+}
+
+fn doc_link_label<'a>(title: &'a str, search_term: &'a str) -> impl egui::Widget + 'a { // 
+    let label = format!("{}:", title);
+    let url = format!("https://docs.rs/egui?search={}", search_term);
+    move |ui: &mut egui::Ui| {
+        ui.hyperlink_to(label, url).on_hover_ui(|ui| {
+            ui.horizontal_wrapped(|ui| {
+                ui.label("Search egui docs for");
+                ui.code(search_term);
+            });
+        })
+    }
+}
+
 impl eframe::App for UIToolkitDemo {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ui.heading("AvdanOS UI Toolkit Demo");
             ui.end_row();
             
-            ui.horizontal(|ui: &mut Ui| {
-                ui.label("Hello, AvdanOS Community!");
-                ui.label("Check out our GitHub !");
+            ui.hyperlink_to(
+                format!("{} Check us out on GitHub !", GITHUB),
+                "https://github.com/Avdan-OS",
+            );
+
+            ui.separator();
+
+            ui.vertical_centered(|ui| {
+                let tooltip_text = "The full egui documentation.\nYou can also click the different widgets names in the left column.";
+                ui.hyperlink("https://docs.rs/egui/").on_hover_text(tooltip_text);
             });
-            ui.hyperlink("https://github.com/Avdan-OS");
+
+            ui.end_row();
+            let mut string = "";
+            ui.add(egui::TextEdit::singleline(&mut string).hint_text("Write something here"));
+
             ui.end_row();
             
             ui.separator();
 
-            ui.horizontal(|ui| { // buttons
+            ui.horizontal(|ui| { // light mode and dark mode buttons
                 ui.label("Dark mode or Light mode ?!");
                 if ui.add(egui::Button::new("Dark mode!")).clicked() {
                     ctx.set_visuals(egui::Visuals::dark());
@@ -38,82 +111,51 @@ impl eframe::App for UIToolkitDemo {
                 if ui.add(egui::Button::new("Light mode!")).clicked() {
                     ctx.set_visuals(egui::Visuals::light());
                 }
-            }); // buttons
+            }); // light mode and dark mode buttons
 
-            ui.collapsing("Click to see sliders !", |ui| { // sliders 
-                ui.label("AvdanOS Coolness : ");
-                ui.add(egui::Slider::new(&mut 0, 0..=1000),);
-                ui.label("Rust Coolness : ");
-                ui.add(egui::Slider::new(&mut 0, 0..=1000));
-            }); // sliders 
+            ui.add(doc_link_label(
+                "SelectableLabel",
+                "selectable_value, SelectableLabel",
+            ));
+    
+            ui.add(doc_link_label("ComboBox", "ComboBox"));
 
-            ui.collapsing("Click to see checkboxes !", |ui| { // checkboxes
-                ui.checkbox(&mut true, "AvdanOS is cool !");
-                ui.checkbox(&mut true, "We need more developers !");
-            }); // sliders
+            #[cfg(feature = "chrono")]
+            {
+                let date = date.get_or_insert_with(|| chrono::offset::Utc::now().date());
+                ui.add(doc_link_label::<'a>("DatePickerButton", "DatePickerButton"));
+                ui.add(egui_extras::DatePickerButton::new(date));
+                ui.end_row();
+            }
 
-            ui.collapsing("Click to see a dragvalue !", |ui| { // drag values
-                ui.label("Dragvalues :");
-                ui.add(egui::DragValue::new(&mut 1).speed(0.1));
-                ui.add(egui::DragValue::new(&mut 10).speed(1.5));
-            }); // drag values
-            
-            ui.collapsing("Click to see what is hidden!", |ui| { // hidden thing
-                ui.label("Not much, as it turns out");
-            }); // hidden thing
-            
-            ui.collapsing("Counting Thing!", |ui| { // counting thing
-                let counter = &mut 1;
-                ui.label("Count and stuff :");
-                ui.horizontal(|ui| {
-                    if ui.button("-").clicked() {
-                        *counter -= 1;
-                    }
-                    ui.label(counter.to_string());
-                    if ui.button("+").clicked() {
-                        *counter += 1;
-                    }
-                }); // ui horizontal
-            }); // counting thing
+            ui.add(doc_link_label("Separator", "separator"));
             ui.separator();
-        }); // central panel
+            ui.end_row();
+
+            ui.add(doc_link_label("CollapsingHeader", "collapsing"));
+            ui.collapsing("Click to see what is hidden!", |ui| {
+                ui.horizontal_wrapped(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label("It's a ");
+                    ui.add(doc_link_label("Spinner", "spinner"));
+                    ui.add_space(4.0);
+                    ui.add(egui::Spinner::new());
+                });
+            });
+            ui.end_row();
+
+            ui.separator();
+
+            ui.add(doc_link_label("ProgressBar", "ProgressBar"));
+            let progress = self.scalar / 360.0;
+            let progress_bar = egui::ProgressBar::new(progress)
+                .show_percentage()
+                .animate(self.animate_progress_bar);
+            self.animate_progress_bar = ui
+                .add(progress_bar)
+                .on_hover_text("The progress bar can be animated!")
+                .hovered();
+            ui.end_row();
+            }); // central panel
     }
-
-    // those are the default members don't touch them (they're pretty much helper functions so yeah)
-    fn save(&mut self, _storage: &mut dyn eframe::Storage) {}
-
-    fn on_exit_event(&mut self) -> bool {
-        true
-    }
-
-    fn on_exit(&mut self, _gl: &eframe::glow::Context) {}
-
-    fn auto_save_interval(&self) -> std::time::Duration {
-        std::time::Duration::from_secs(30)
-    }
-
-    fn max_size_points(&self) -> egui::Vec2 {
-        egui::Vec2::INFINITY
-    }
-
-    fn clear_color(&self, _visuals: &egui::Visuals) -> egui::Rgba {
-        // NOTE: a bright gray makes the shadows of the windows look weird.
-        // We use a bit of transparency so that if the user switches on the
-        // `transparent()` option they get immediate results.
-        egui::Color32::from_rgba_unmultiplied(12, 12, 12, 180).into()
-
-        // _visuals.window_fill() would also be a natural choice
-    }
-
-    fn persist_native_window(&self) -> bool {
-        true
-    }
-
-    fn persist_egui_memory(&self) -> bool {
-        true
-    }
-
-    fn warm_up_enabled(&self) -> bool {
-        false
-    } // central panel
 }
