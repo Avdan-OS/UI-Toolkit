dea752a65074f9bb4265da46b066cae26eff0b0d
Daniel D <kibalchishthecoder@gmail.com>
2022-08-25 14:33:47 +0300

    Finished all of the UI components and fixed bugs

--- README.md
+++ README.md
@@ -18,18 +18,18 @@ On debian-based linux distros run this command on the terminal:
 
 ```sudo apt-get install -y libclang-dev libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev```
 
-Meanwhile on fedora-based distros you need to run this command:
+Meanwhile on Fedora-based distros, you need to run this command:
 
 ```sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel```
 
 ### Step 2: Building and running
 
-1. Clone this repository(`git clone https://github.com/Avdan-OS/UI-Toolkit.git`)
+1. Clone this repository : (`git clone https://github.com/Avdan-OS/UI-Toolkit.git`)
 
-2. Build the cloned repo(`cd UI-Toolkit && cargo run`)
+2. Build the cloned repo : (`cd UI-Toolkit && cargo run`)
 
-3. You're done!
+3. Yay! You're done!
 
-### Useful Resources 
+### Useful Resources
 
 Explore the different eGUI Widgets : `https://www.egui.rs/index.html#demo`
--- src/toolkit.rs
+++ src/toolkit.rs
@@ -1,16 +1,33 @@
-use eframe::egui::{self, Response};
+use eframe::egui::{self, Response, Widget};
 use eframe::egui::special_emojis::GITHUB;
 use eframe::egui::style::WidgetVisuals;
-use egui::plot::{Line, Plot, Value, Values};
+use egui::plot::*;
+use egui_extras;
+
 
 use crate::egui::Ui;
 use crate::fonts::setup_font;
 
 #[derive(Default)]
 pub struct UIToolkitDemo{
+    enabled: bool,
+    visible: bool,
+    boolean: bool,
+    radio: Enum,
     scalar: f32,
+    string: String,
+    color: egui::Color32,
     animate_progress_bar: bool,
+    text_input:String, 
+
+    #[cfg(feature = "chrono")]
+    #[cfg_attr(feature = "serde", serde(skip))]
+    date: Option<chrono::Date<chrono::Utc>>,
+
+    #[cfg_attr(feature = "serde", serde(skip))]
+    texture: Option<egui::TextureHandle>,
 }
+
 
 impl UIToolkitDemo {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -19,17 +36,15 @@ impl UIToolkitDemo {
     }
 }
 
-
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[derive(Default)]
 enum Enum {
+    #[default]
     First,
     Second,
     Third,
 }
-
-/// Shows off one example of each major type of widget.
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct WidgetGallery {
     enabled: bool,
     visible: bool,
@@ -39,13 +54,6 @@ pub struct WidgetGallery {
     string: String,
     color: egui::Color32,
     animate_progress_bar: bool,
-
-    #[cfg(feature = "chrono")]
-    #[cfg_attr(feature = "serde", serde(skip))]
-    date: Option<chrono::Date<chrono::Utc>>,
-
-    #[cfg_attr(feature = "serde", serde(skip))]
-    texture: Option<egui::TextureHandle>,
 }
 
 impl Default for WidgetGallery {
@@ -59,7 +67,6 @@ impl Default for WidgetGallery {
             string: Default::default(),
             color: egui::Color32::LIGHT_BLUE.linear_multiply(0.5),
             animate_progress_bar: true,
-            texture: None,
         }
     }
 }
@@ -77,21 +84,6 @@ fn doc_link_label<'a>(title: &'a str, search_term: &'a str) -> impl egui::Widget
         })
     }
 }
-
-fn example_plot(ui: &mut egui::Ui) -> egui::Response { // the sine plotting function 
-    use egui::plot::{Line, Plot, Value, Values};
-    let sin = (0..1000).map(|i| {
-        let x = i as f64 * 0.01;
-        Value::new(x, x.sin())
-    });
-    let line = Line::new(Values::from_values_iter(sin));
-
-    Plot::new("plot")
-        .view_aspect(1.0)
-        .height(45.0)
-        .show(ui, |plot_ui| plot_ui.line(line))
-        .response
-}
 impl eframe::App for UIToolkitDemo {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui: &mut Ui| {
@@ -102,23 +94,17 @@ impl eframe::App for UIToolkitDemo {
                 format!("{} Check us out on GitHub !", GITHUB),
                 "https://github.com/Avdan-OS",
             );
-
-            ui.separator();
-
+            
             ui.vertical_centered(|ui| {
                 let tooltip_text = "The full egui documentation.\nYou can also click the different widgets names in the left column.";
                 ui.hyperlink("https://docs.rs/egui/").on_hover_text(tooltip_text);
             });
-
-            ui.end_row();
-            let mut string:String = "".to_string();
-            ui.add(egui::TextEdit::singleline(&mut string).hint_text("Write something here"));
-
-            ui.end_row();
             
             ui.separator();
+            ui.end_row();
 
-            ui.horizontal(|ui| { // light mode and dark mode buttons
+            // light mode and dark mode buttons
+            ui.horizontal(|ui| { 
                 ui.label("Dark mode or Light mode ?!");
                 if ui.add(egui::Button::new("Dark mode!")).clicked() {
                     ctx.set_visuals(egui::Visuals::dark());
@@ -126,67 +112,98 @@ impl eframe::App for UIToolkitDemo {
                 if ui.add(egui::Button::new("Light mode!")).clicked() {
                     ctx.set_visuals(egui::Visuals::light());
                 }
-            }); // light mode and dark mode buttons
+            });
+            ui.end_row();
 
+            // Input box
+            ui.add(egui::TextEdit::singleline(&mut self.text_input).hint_text("Write something here"));
+            ui.end_row();
+            
+            // Slider 
             ui.add(doc_link_label("Slider", "Slider"));
             ui.add(egui::Slider::new(&mut self.scalar, 0.0..=360.0).suffix("°"));
             ui.end_row();
-
+            
+            // Drag Value
             ui.add(doc_link_label("DragValue", "DragValue"));
             ui.add(egui::DragValue::new(&mut self.scalar).speed(1.0));
             ui.end_row();
+            
+            // Progress Bar
+            ui.add(doc_link_label("ProgressBar", "ProgressBar"));
+            let progress = self.scalar / 360.0;
+            let progress_bar = egui::ProgressBar::new(progress)
+                .show_percentage()
+                .animate(self.animate_progress_bar);
+            
+            self.animate_progress_bar = ui
+                .add(progress_bar)
+                .on_hover_text("The progress bar can be animated!")
+                .hovered();
+            ui.end_row();
 
+            // Color Picker
+            ui.add(doc_link_label("Color picker", "color_edit"));
+            ui.color_edit_button_srgba(&mut self.color);
+            ui.end_row();
+
+            // "Click me !" Button
+            ui.add(doc_link_label("Link", "link"));
+            if ui.link("Click me!").clicked() {
+                *&mut self.boolean = !*&mut self.boolean;
+            }
+            ui.end_row();
+
+            // Checkbox
+            ui.add(doc_link_label("Checkbox", "checkbox"));
+            ui.checkbox(&mut self.boolean, "Checkbox");
+            ui.end_row();
+
+            // Radio Button 
+            ui.add(doc_link_label("RadioButton", "radio"));
+            ui.horizontal(|ui| {
+                ui.radio_value(&mut self.radio, Enum::First, "First");
+                ui.radio_value(&mut self.radio, Enum::Second, "Second");
+                ui.radio_value(&mut self.radio, Enum::Third, "Third");
+            });
+            ui.end_row();
+
+            // Selectable Label
             ui.add(doc_link_label(
                 "SelectableLabel",
                 "selectable_value, SelectableLabel",
             ));
-    
+            ui.horizontal(|ui| {
+                ui.selectable_value(&mut self.radio, Enum::First, "First");
+                ui.selectable_value(&mut self.radio, Enum::Second, "Second");
+                ui.selectable_value(&mut self.radio, Enum::Third, "Third");
+            });
+            ui.end_row();
+            
+            // ComboBox
             ui.add(doc_link_label("ComboBox", "ComboBox"));
-
-            #[cfg(feature = "chrono")]
-            {
-                let date = date.get_or_insert_with(|| chrono::offset::Utc::now().date());
-                ui.add(doc_link_label::<'a>("DatePickerButton", "DatePickerButton"));
-                ui.add(egui_extras::DatePickerButton::new(date));
-                ui.end_row();
-            }
-
-            ui.add(doc_link_label("Separator", "separator"));
-            ui.separator();
+            egui::ComboBox::from_label("Take your pick")
+                .selected_text(format!("{:?}", &mut self.radio))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut self.radio, Enum::First, "First");
+                    ui.selectable_value(&mut self.radio, Enum::Second, "Second");
+                    ui.selectable_value(&mut self.radio, Enum::Third, "Third");
+                });
             ui.end_row();
 
+            // Collapsing Header + Spinner 
             ui.add(doc_link_label("CollapsingHeader", "collapsing"));
             ui.collapsing("Click to see what is hidden!", |ui| {
                 ui.horizontal_wrapped(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
                     ui.label("It's a ");
-                    ui.add(doc_link_label("Spinner", "spinner"));
+                    ui.add(doc_link_label("Spinner ! ", "spinner"));
                     ui.add_space(4.0);
                     ui.add(egui::Spinner::new());
                 });
             });
             ui.end_row();
-
             ui.separator();
-
-            
-            ui.separator();
-            
-            ui.add(doc_link_label("ProgressBar", "ProgressBar"));
-            
-            let progress = self.scalar / 360.0;
-            let progress_bar = egui::ProgressBar::new(progress)
-            .show_percentage()
-            .animate(self.animate_progress_bar);
-            
-            self.animate_progress_bar = ui
-            .add(progress_bar)
-            .on_hover_text("The progress bar can be animated!")
-            .hovered();
-            
-            ui.end_row();
-
-            example_plot(ui); // the plot 
-            }); // central panel
+        });
     }
 }
