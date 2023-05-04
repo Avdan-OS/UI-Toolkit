0a43fa3dc68aa6ef419f668e8d4265dbb652cb4f
Akane <128232444+mrakane7@users.noreply.github.com>
2023-05-04 19:57:10 +0200

    Update fonts.rs

--- src/fonts.rs
+++ src/fonts.rs
@@ -1,27 +1,48 @@
-pub(crate) use eframe::egui::{
-    FontDefinitions,
-    self,
+#![allow(dead_code)] // temporary
+
+use iced::Font;
+
+pub const BLACK: Font = Font::External {
+    name: "Inter Black",
+    bytes: include_bytes!("../assets/fonts/Inter-Black.ttf")
 };
 
-pub fn setup_font(ctx: &egui::Context) {
-    let mut font: FontDefinitions = egui::FontDefinitions::default();
+pub const BOLD: Font = Font::External {
+    name: "Inter Bold",
+    bytes: include_bytes!("../assets/fonts/Inter-Bold.ttf")
+};
 
-    font.font_data.insert (
-        "inter".to_owned(),
-        egui::FontData::from_static(include_bytes!("../assets/fonts/Inter-Regular.ttf")),
-    );
+pub const EXTRA_BOLD: Font = Font::External {
+    name: "Inter Extra Bold",
+    bytes: include_bytes!("../assets/fonts/Inter-ExtraBold.ttf")
+};
 
-    font
-        .families
-        .entry(egui::FontFamily::Proportional)
-        .or_default()
-        .insert(0, "inter".to_owned());
+pub const EXTRA_LIGHT: Font = Font::External {
+    name: "Inter Extra Light",
+    bytes: include_bytes!("../assets/fonts/Inter-ExtraLight.ttf")
+};
 
-    font
-        .families
-        .entry(egui::FontFamily::Monospace)
-        .or_default()
-        .push("inter".to_owned());
+pub const LIGHT: Font = Font::External {
+    name: "Inter Light",
+    bytes: include_bytes!("../assets/fonts/Inter-Light.ttf")
+};
 
-    ctx.set_fonts(font); // sets inter the default font
-}
+pub const MEDIUM: Font = Font::External {
+    name: "Inter Medium",
+    bytes: include_bytes!("../assets/fonts/Inter-Medium.ttf")
+};
+
+pub const REGULAR: Font = Font::External {
+    name: "Inter Regular",
+    bytes: include_bytes!("../assets/fonts/Inter-Regular.ttf")
+};
+
+pub const SEMI_BOLD: Font = Font::External {
+    name: "Inter Semi Bold",
+    bytes: include_bytes!("../assets/fonts/Inter-SemiBold.ttf")
+};
+
+pub const THIN: Font = Font::External {
+    name: "Inter Thin",
+    bytes: include_bytes!("../assets/fonts/Inter-Thin.ttf")
+};
