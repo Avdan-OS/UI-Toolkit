0e04bc1e783dd55ac95c310d6cebc79a85dfe8fb
Akane <128232444+mrakane7@users.noreply.github.com>
2023-05-04 19:57:30 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,74 @@
-use eframe::*;
-use toolkit::UIToolkitDemo;
+#![allow(unused_imports)] // temporary
 
-mod fonts;   // fonts file
-mod toolkit; // the actual app code
+use iced::widget::{
+    column,
+    container,
+    text
+};
 
-pub(crate) fn main() {
-    let native_options: NativeOptions = eframe::NativeOptions::default();
+use iced::{
+    Alignment,
+    Element,
+    Length,
+    Sandbox,
+    Settings
+};
 
-    eframe::run_native (
-        "AvdanOS UI Toolkit Demo",
-        native_options,
-        Box::new(|cc: &CreationContext| Box::new(UIToolkitDemo::new(cc)))
-    );
+mod fonts;
+
+use crate::fonts::{
+    BLACK,
+    BOLD,
+    EXTRA_BOLD,
+    EXTRA_LIGHT,
+    LIGHT,
+    MEDIUM,
+    REGULAR,
+    SEMI_BOLD,
+    THIN
+};
+
+pub fn main() -> iced::Result {
+    Toolkit::run(Settings::default()) // run program
+}
+
+struct Toolkit {}
+
+// interactions
+#[derive(Debug, Clone, Copy)]
+enum Message {}
+
+impl Sandbox for Toolkit {
+    type Message = Message;
+
+    // store interactions
+    fn new() -> Self {
+        Self {}
+    }
+
+    // window title
+    fn title(&self) -> String {
+        String::from("AvdanOS UI Toolkit Demo")
+    }
+
+    // update state
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    // show things on screen
+    fn view(&self) -> Element<Message> {
+        let title = text("AvdanOS UI Toolkit Demo")
+            .font(REGULAR);
+
+        let content = column![title]
+            .padding(20)
+            .align_items(Alignment::Start);
+
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .into()
+    }
 }
